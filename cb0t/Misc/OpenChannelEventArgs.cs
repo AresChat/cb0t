5d762223e1e6ce8037cd78aab52e1b0fde4d05dc
oobenoob_cp <SND\oobenoob_cp>
2013-10-07 17:27:09 +0000

    channel list.

---
+++ cb0t/Misc/OpenChannelEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cb0t
+{
+    public class OpenChannelEventArgs : EventArgs
+    {
+        public FavouritesListItem Room { get; set; }
+    }
+}
