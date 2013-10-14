5b5449fb0ebefbec02ec7a3bf94bd97ee4735b73
oobenoob_cp <SND\oobenoob_cp>
2013-10-14 15:30:15 +0000

    socket added.

--- cb0t/RoomPanel/Room.cs
+++ cb0t/RoomPanel/Room.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Net.Sockets;
 
 namespace cb0t
 {
@@ -13,8 +14,25 @@ namespace cb0t
         public RoomPanel Panel { get; set; }
         public IPEndPoint EndPoint { get; set; }
 
+        private SessionState state = SessionState.Sleeping;
+        private int reconnect_count = 0;
+        private AresSocket sock = new AresSocket();
+        private uint ticks = 0;
+
+        public Room(uint time, FavouritesListItem item)
+        {
+            this.Credentials = item;
+            this.EndPoint = new IPEndPoint(item.IP, item.Port);
+            this.ticks = (time - 19);
+            this.sock.PacketReceived += this.PacketReceived;
+        }
+
         public void Release()
         {
+            this.sock.Disconnect();
+            this.sock.PacketReceived -= this.PacketReceived;
+            this.sock.Free();
+            this.sock = null;
             this.Button.Free();
             this.Button.Dispose();
             this.Button = null;
@@ -25,7 +43,57 @@ namespace cb0t
 
         public void SocketTasks(uint time)
         {
-            this.Panel.thest();
+            if (this.state == SessionState.Sleeping)
+            {
+                if (time >= (this.ticks + 20))
+                {
+                    this.state = SessionState.Connecting;
+                    this.sock.Connect(this.EndPoint);                    
+                    this.ticks = time;
+
+                    if (this.reconnect_count > 0)
+                        this.Panel.ServerText("Connecting to host, please wait... #" + this.reconnect_count);
+                    else
+                        this.Panel.ServerText("Connecting to host, please wait...");
+                }
+            }
+            else if (this.state == SessionState.Connecting)
+            {
+                if (this.sock.Connected)
+                {
+                    this.state = SessionState.Connected;
+                    this.ticks = time;
+                    this.Panel.ServerText("Connected, handshaking...");
+                }
+                else if (time >= (this.ticks + 10))
+                {
+                    this.ticks = time;
+                    this.state = SessionState.Sleeping;
+                    this.sock.Disconnect();
+                    this.reconnect_count++;
+                    this.Panel.AnnounceText("Unable to connect");
+                }
+            }
+            else if (!this.sock.Service())
+            {
+                this.ticks = time;
+                this.state = SessionState.Sleeping;
+                this.sock.Disconnect();
+                this.reconnect_count++;
+                this.Panel.AnnounceText("Disconnected (remote connection reset)");
+            }
         }
+
+        private void PacketReceived(object sender, PacketReceivedEventArgs e)
+        {
+            switch (e.Msg)
+            {
+                default:
+                    this.Panel.AnnounceText(e.Msg.ToString());
+                    break;
+            }
+        }
+
+
     }
 }
---
+++ cb0t/TCPPacketReader.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace cb0t
+{
+    class TCPPacketReader
+    {
+        private int Position = 0;
+        private List<byte> Data = new List<byte>();
+
+        public TCPPacketReader(byte[] bytes)
+        {
+            this.Data.Clear();
+            this.Position = 0;
+            this.Data.AddRange(bytes);
+        }
+
+        public int Remaining
+        {
+            get { return this.Data.Count - this.Position; }
+        }
+
+        public void SkipByte()
+        {
+            this.Position++;
+        }
+
+        public void SkipBytes(int count)
+        {
+            this.Position += count;
+        }
+
+        public static implicit operator byte(TCPPacketReader p)
+        {
+            byte tmp = p.Data[p.Position];
+            p.Position++;
+            return tmp;
+        }
+
+        public static implicit operator byte[](TCPPacketReader p)
+        {
+            byte[] tmp = new byte[p.Data.Count - p.Position];
+            Array.Copy(p.Data.ToArray(), p.Position, tmp, 0, tmp.Length);
+            p.Position += tmp.Length;
+            return tmp;
+        }
+
+        public static implicit operator Guid(TCPPacketReader p)
+        {
+            byte[] tmp = new byte[16];
+            Array.Copy(p.Data.ToArray(), p.Position, tmp, 0, tmp.Length);
+            p.Position += 16;
+            return new Guid(tmp);
+        }
+
+        public static implicit operator ushort(TCPPacketReader p)
+        {
+            ushort tmp = BitConverter.ToUInt16(p.Data.ToArray(), p.Position);
+            p.Position += 2;
+            return tmp;
+        }
+
+        public static implicit operator uint(TCPPacketReader p)
+        {
+            uint tmp = BitConverter.ToUInt32(p.Data.ToArray(), p.Position);
+            p.Position += 4;
+            return tmp;
+        }
+
+        public static implicit operator ulong(TCPPacketReader p)
+        {
+            ulong tmp = BitConverter.ToUInt64(p.Data.ToArray(), p.Position);
+            p.Position += 8;
+            return tmp;
+        }
+
+        public static implicit operator IPAddress(TCPPacketReader p)
+        {
+            byte[] tmp = new byte[4];
+            Array.Copy(p.Data.ToArray(), p.Position, tmp, 0, tmp.Length);
+            p.Position += 4;
+            return new IPAddress(tmp);
+        }
+
+        public byte[] ReadBytes(int count)
+        {
+            byte[] tmp = new byte[count];
+            Array.Copy(this.Data.ToArray(), this.Position, tmp, 0, tmp.Length);
+            this.Position += tmp.Length;
+            return tmp;
+        }
+
+        public String ReadString()
+        {
+          /*  if (Crypto.Mode == CryptoMode.Encrypted)
+            {
+                ushort size = this;
+                byte[] data = this.ReadBytes(size);
+                data = Crypto.Decrypt(data);
+                String str = Encoding.UTF8.GetString(data);
+
+                if (this.Position < this.Data.Count)
+                    if (this.Data[this.Position] == 0)
+                        this.Position++;
+
+                return str;
+            }
+            else
+            {*/
+                String str = String.Empty;
+                int split = this.Data.IndexOf(0, this.Position);
+                byte[] tmp = new byte[split > -1 ? (split - this.Position) : (this.Data.Count - this.Position)];
+                Array.Copy(this.Data.ToArray(), this.Position, tmp, 0, tmp.Length);
+                this.Position = split > -1 ? (split + 1) : this.Data.Count;
+                return Encoding.UTF8.GetString(tmp).Replace("\r\n", "\n");
+           // }
+        }
+
+        public byte[] ToArray()
+        {
+            return this.Data.ToArray();
+        }
+    }
+}
