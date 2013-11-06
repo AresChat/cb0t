0b05861a936782b53a868d735d89d4542fa6629e
oobenoob_cp <SND\oobenoob_cp>
2013-11-06 22:43:42 +0000

    audio player.

---
+++ cb0t/AudioPanel/AudioHelpers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WMPLib;
+
+namespace cb0t
+{
+    class AudioHelpers
+    {
+        public static AudioPlayerItem CreateAudioPlayerItem(WindowsMediaPlayer wmp, String path)
+        {
+            AudioPlayerItem item = new AudioPlayerItem();
+            
+            try
+            {
+                IWMPMedia media = wmp.newMedia(path);
+
+                if (media.duration > 0)
+                {
+                    item.Duration = (int)media.duration;
+                    item.SetDurationText(item.Duration);
+                    item.Title = media.name.Trim();
+                    item.Path = path;
+
+                    for (int i = 0; i < media.attributeCount; i++)
+                    {
+                        switch (media.getAttributeName(i).ToUpper())
+                        {
+                            case "ALBUMIDALBUMARTIST":
+                                String[] strs = media.getItemInfo(media.getAttributeName(i)).Split(new String[] { "*;*" }, StringSplitOptions.None);
+
+                                if (strs.Length > 0)
+                                    item.Album = strs[0].Trim();
+                                if (strs.Length > 1)
+                                    item.Artist = strs[1].Trim();
+                                break;
+
+                            case "AUTHOR":
+                                item.Author = media.getItemInfo(media.getAttributeName(i)).Trim();
+                                break;
+
+                            case "DISPLAYARTIST":
+                                if (item.Artist.Length == 0)
+                                    item.Artist = media.getItemInfo(media.getAttributeName(i)).Trim();
+                                break;
+
+                            case "TITLE":
+                                if (item.Title.Length == 0)
+                                    item.Title = media.getItemInfo(media.getAttributeName(i)).Trim();
+                                break;
+
+                            case "WM/ALBUMARTIST":
+                                if (item.Artist.Length == 0)
+                                    item.Artist = media.getItemInfo(media.getAttributeName(i)).Trim();
+                                break;
+
+                            case "WM/ALBUMTITLE":
+                                if (item.Album.Length == 0)
+                                    item.Album = media.getItemInfo(media.getAttributeName(i)).Trim();
+                                break;
+                        }
+                    }
+
+                    if (String.IsNullOrEmpty(item.Artist))
+                        item.Artist = item.Author;
+                }
+            }
+            catch { return null; }
+
+            return item;
+        }
+    }
+}
--- cb0t/AudioPanel/AudioPanel.Designer.cs
+++ cb0t/AudioPanel/AudioPanel.Designer.cs
@@ -28,46 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.audioList1 = new cb0t.AudioList();
+            this.albumArtBox1 = new cb0t.AlbumArtBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.albumArtBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.albumArtBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 441);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(178, 23);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 214);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "audio";
+            this.label1.Size = new System.Drawing.Size(174, 148);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // label2
+            // audioList1
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 137);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "audio";
+            this.audioList1.BackColor = System.Drawing.Color.White;
+            this.audioList1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.audioList1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.audioList1.FullRowSelect = true;
+            this.audioList1.GridLines = true;
+            this.audioList1.Location = new System.Drawing.Point(178, 0);
+            this.audioList1.Name = "audioList1";
+            this.audioList1.OwnerDraw = true;
+            this.audioList1.Size = new System.Drawing.Size(559, 441);
+            this.audioList1.TabIndex = 1;
+            this.audioList1.UseCompatibleStateImageBehavior = false;
+            this.audioList1.View = System.Windows.Forms.View.Details;
+            // 
+            // albumArtBox1
+            // 
+            this.albumArtBox1.Location = new System.Drawing.Point(2, 25);
+            this.albumArtBox1.Name = "albumArtBox1";
+            this.albumArtBox1.Size = new System.Drawing.Size(174, 174);
+            this.albumArtBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.albumArtBox1.TabIndex = 0;
+            this.albumArtBox1.TabStop = false;
             // 
             // AudioPanel
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.audioList1);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "AudioPanel";
+            this.Size = new System.Drawing.Size(737, 441);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.AudioPanel_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.AudioPanel_DragEnter);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.albumArtBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private AlbumArtBox albumArtBox1;
+        private AudioList audioList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+
     }
 }
