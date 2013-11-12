6dd8b1fd5079f83bf3942ad8e8732d452ff28223
oobenoob_cp <SND\oobenoob_cp>
2013-11-12 23:07:09 +0000

    protocol.

---
+++ cb0t/RoomPanel/ClientFeatures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cb0t
+{
+    public enum ClientFeatures : byte
+    {
+        CLIENT_SUPPORTS_VC = 1,
+        CLIENT_SUPPORTS_PM_VC = 2,
+        CLIENT_SUPPORTS_OPUS_VC = 4,
+        CLIENT_SUPPORTS_OPUS_PM_VC = 8,
+        CLIENT_SUPPORTS_HTML = 16
+    }
+}
---
+++ cb0t/RoomPanel/ServerFeatures.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cb0t
+{
+    public enum ServerFeatures : byte
+    {
+        SERVER_SUPPORTS_PVT = 1,
+        SERVER_SUPPORTS_SHARING = 2,
+        SERVER_SUPPORTS_COMPRESSION = 4,
+        SERVER_SUPPORTS_VC = 8,
+        SERVER_SUPPORTS_OPUS_VC = 16,
+        SERVER_SUPPORTS_ROOM_SCRIBBLES = 32,
+        SERVER_SUPPORTS_PM_SCRIBBLES = 64,
+        SERVER_SUPPORTS_HTML = 128
+    }
+}
