cae06b06ae23f1e6087c19f192fe995ae6c0981c
oobenoob_cp <SND\oobenoob_cp>
2013-12-08 15:54:30 +0000

    3.05 source code.

--- cb0t/Misc/Settings.cs
+++ cb0t/Misc/Settings.cs
@@ -13,7 +13,7 @@ namespace cb0t
     class Settings
     {
         public const String APP_NAME = "cb0t";
-        public const String APP_VERSION = "3.04";
+        public const String APP_VERSION = "3.05";
 
         public static bool CAN_WRITE_REG { get; set; }
         public static bool IsAway { get; set; }
