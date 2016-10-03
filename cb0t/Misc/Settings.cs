9e0c94307b21ac2c2a17c6cb77b51cb612b74ab4
Stuart <stuart@programmer.net>
2016-10-03 16:37:37 +0100

    update version to 3.36

--- cb0t/Misc/Settings.cs
+++ cb0t/Misc/Settings.cs
@@ -13,7 +13,7 @@ namespace cb0t
     class Settings
     {
         public const String APP_NAME = "cb0t";
-        public const String APP_VERSION = "3.35";
+        public const String APP_VERSION = "3.36";
 
         public static bool CAN_WRITE_REG { get; set; }
         public static bool IsAway { get; set; }
