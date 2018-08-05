23faf330308707414392cd0cc6e905b809d9a330
Deanz0! <ohgoshdean@users.noreply.github.com>
2018-08-05 21:28:40 +0100

    Create Spotify.cs
    Spotify listner

---
+++ cb0t/MediaIPC/Spotify/Spotify.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+
+namespace MediaIPC.Spotify
+{
+    class Spotify
+    {
+        public String Song
+        {
+            get
+            {
+                Process[] ps = Process.GetProcessesByName("spotify");
+
+                if (ps.Length > 0)
+                {
+                    String str = ps[0].MainWindowTitle;
+                    return str;
+                }
+
+                return null;
+            }
+        }
+    }
+}
